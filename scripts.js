1a81d13c8973924e111ff5fbe49592b453b4d598
JoaoPJPrioli <111326485+JoaoPJPrioli@users.noreply.github.com>
2024-11-20 14:55:59 -0500

    Delete scripts.js

--- scripts.js
+++
@@ -1,4 +0,0 @@
-// Add interactivity if needed (e.g., dynamic team member loading or research updates)
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Website loaded successfully!");
-});
